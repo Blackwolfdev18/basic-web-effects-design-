66ad47ac540090fc48c389d62b40c365f512fcaa
ggpio18 <58065866+ggpio18@users.noreply.github.com>
2024-10-05 00:59:22 +0800

    updt js

--- code/Dynamic table/script.js
+++ code/Dynamic table/script.js
@@ -13,3 +13,31 @@ function checkValues() {
       createTable(row.value, col.value);
     }
   }
+
+  function createTable(rows, cols) {
+    var table = document.getElementById("table");
+    table.innerHTML = "";
+
+    var header = table.createTHead();
+    var headRow = header.insertRow();
+    for (var j = 0; j < cols; j++) {
+      var headCell = document.createElement("th");
+      headCell.innerHTML =
+        '<input type="text" class="cell-val" placeholder="Column Name" />';
+
+      headRow.appendChild(headCell);
+    }
+
+    var body = table.createTBody();
+    for (var i = 0; i < rows - 1; i++) {
+      var row = body.insertRow();
+      for (var j = 0; j < cols; j++) {
+        var cell = row.insertCell();
+        cell.innerHTML =
+          '<input type="text" class="cell-val" placeholder="Value" />';
+      }
+    }
+
+    document.getElementById("set-vals").style.display = "block";
+    document.querySelector(".table-container").style.display = "block";
+  }
